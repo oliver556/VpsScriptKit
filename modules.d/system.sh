2227667c075be7264186f447b7d0d3611cacfaeb
Jamison <aligoogle@163.com>
2025-07-26 21:21:18 +0800

    fix: 【⚙️  系统工具】修改间距

--- modules.d/system.sh
+++ modules.d/system.sh
@@ -51,8 +51,8 @@ system_menu() {
         echo -e "${BOLD_GREY}13. ${WHITE}用户管理             ${BOLD_GREY}14. ${WHITE}用户/密码生成器"
         print_echo_line_1
         echo -e "${LIGHT_CYAN}15. ${WHITE}修改主机名           ${LIGHT_CYAN}16. ${WHITE}修改系统时区         ${BOLD_GREY}17. ${WHITE}设置BBR3加速"
-        echo -e "${BOLD_GREY}18. ${WHITE}防火墙高级管理器     ${BOLD_GREY}19. ${WHITE}iptables一键转发       ${BOLD_GREY}20. ${WHITE}NAT批量SSH连接测试"
-        echo -e "${BOLD_GREY}21. ${WHITE}切换系统更新源       ${BOLD_GREY}22. ${WHITE}定时任务管理       ${BOLD_GREY}23. ${WHITE}ip开放端口扫描"
+        echo -e "${BOLD_GREY}18. ${WHITE}防火墙高级管理器     ${BOLD_GREY}19. ${WHITE}iptables一键转发     ${BOLD_GREY}20. ${WHITE}NAT批量SSH连接测试"
+        echo -e "${BOLD_GREY}21. ${WHITE}切换系统更新源       ${BOLD_GREY}22. ${WHITE}定时任务管理         ${BOLD_GREY}23. ${WHITE}ip开放端口扫描"
         print_echo_line_1
         echo -e "${BOLD_GREY}55. ${WHITE}设置脚本启动快捷键"
         echo -e "${BOLD_GREY}66. ${WHITE}一条龙系统调优"
